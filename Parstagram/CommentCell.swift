5c94cf28ee6b4362fd274215c5b64d455ea4176a
dxiong7 <danielxiong523@gmail.com>
2019-04-16 01:20:06 -0700

    Add comments functionality

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,26 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Daniel Xiong on 4/15/19.
+//  Copyright © 2019 danielxiong523. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
